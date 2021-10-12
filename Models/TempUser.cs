46637bc39def7c76bda9599e26054a073c89d8c2
shaiufpritul <62388473+shaiufpritul@users.noreply.github.com>
2021-10-12 22:48:26 +0600

    Add files via upload

---
+++ Models/TempUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DevProject.Models
+{
+    public class TempUser
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string UserEmail { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string UserPass { get; set; }
+
+
+
+    }
+}
---
+++ SQLQueryfin.sql
@@ -0,0 +1,58 @@
+﻿CREATE TABLE [dbo].[Adminn]
+(  
+    [AdminID] INT IDENTITY(1,1) PRIMARY KEY,
+	[UserName] NVARCHAR(50),
+    [UserPass] NVARCHAR(50) NOT NULL,
+	[UserEmail] NVARCHAR(50) NOT NULL,	
+
+)
+
+CREATE TABLE [dbo].[JobFinder]
+(  
+    [JobFindID] INT IDENTITY(1,1) PRIMARY KEY,
+	[UserName] NVARCHAR(50),
+    [UserPass] NVARCHAR(50) NOT NULL,
+	[Address] NVARCHAR(100),
+	[UserEmail] NVARCHAR(50) NOT NULL,
+	[ConNumber] BIGINT NOT NULL,
+	[College] NVARCHAR(100) NULL,
+	[University] NVARCHAR(100) NULL,
+	[Experience] TEXT NULL,
+	[Resume] VARBINARY(MAX) NULL,
+
+)
+
+CREATE TABLE [dbo].[JobPoster]
+(  
+    [JobPostID] INT IDENTITY(1,1) PRIMARY KEY,
+	[UserName] NVARCHAR(50),
+    [UserPass] NVARCHAR(50) NOT NULL,
+	[JobName] NVARCHAR(100) NOT NULL,		
+    [JobDesc] TEXT NULL,
+	[WorkDays] SMALLINT NOT NULL,
+	[Salary] BIGINT NOT NULL,
+	[Skills] TEXT NOT NULL,
+	[Age] SMALLINT NULL,
+	[Gender] NVARCHAR(50) NULL,
+	[Email] NVARCHAR(50),
+    [ConNumber] BIGINT NOT NULL,
+
+)
+
+
+CREATE TABLE [dbo].[Support]
+(  
+     [SupportID] INT IDENTITY(1,1) PRIMARY KEY,
+	 [Name] NVARCHAR(50) NULL,
+	 [EMAIL] NVARCHAR(50) NOT NULL,
+	 [Subject] NVARCHAR(100) NOT NULL,
+	 [Message] TEXT NOT NULL,
+)
+
+CREATE TABLE [dbo].[Apply]
+(
+    [ApplyID] INT IDENTITY(1,1) PRIMARY KEY,
+	[ApplyName] NVARCHAR(100) NULL,
+	[JobFindID] INT NOT NULL FOREIGN KEY REFERENCES JobFinder(JobFindID),
+	[JobPostID] INT NOT NULL FOREIGN KEY REFERENCES JobPoster(JobPostID),
+)
